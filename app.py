1f75eb1659937c5933a13d59b248dece72f5ab72
Marvs0823 <marvinrvillanueva@iskolarngbayan.pup.edu.ph>
2023-08-26 18:12:01 +0800

    Added Features

--- app.py
+++ app.py
@@ -80,7 +80,6 @@ def student_home():
 # ========================================================================
 # ALL FACULTY ROUTES HERE
 @app.route('/faculty')
-@prevent_authenticated
 def faculty_login():
     return render_template('faculty/login.html')
 
